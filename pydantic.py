0fb119e1a69e984214bb2eebca4fdabde016917a
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-09 10:36:53 +0530

    make a patient model using pydantic with the following fields: name (str), age (int), email (EmailStr), and medical_history (List[str]).

--- pydantic.py
+++ pydantic.py
@@ -1,2 +1,8 @@
 from pydantic import BaseModel ,EmailStr, AnyUrl, Field
 from typing import List ,Dict, Optional, Annotated
+
+class patient(BaseModel):
+    name : Annotated[str, Field( max_length=50, tittle = 'name of the patient', description='give the name of the patient in less than 50 words ')]
+    email : EmailStr
+    linkedin_url : AnyUrl
+    age: Annotated[int , Field(gt=0, lt=120, title='age of the patient ', )]
