77c2aa6d9619165b2fe830bdd286baf0b10bfc5c
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-09 12:41:47 +0530

    make a email field validator

--- fielld_validator.py
+++ fielld_validator.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, EmailStr, anyurl , field , field_validator
-from typing import list , dict , optimal , Annotated
+from typing import list , Dict , optimal , Annotated
 
 class Patient(BaseModel):
     
@@ -11,5 +11,14 @@ class Patient(BaseModel):
     allergies : list[str]
     contact_detail: Dict[str, str]
     
+@field_validator('email')
+@classmethod
+def email_validator(cls, value):
+    valid_domain : ['hdfc.com', 'icici.com']
+    domain_name :value.split('@')[-1]
+    if value  not in domain_name:
+       raise ValueError('not a valid domain')
+    return value
+
     
     
