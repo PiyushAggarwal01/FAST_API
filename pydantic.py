7557e2b120cc903bd68cee6b0ecdc692ababca98
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-09 10:52:47 +0530

    pydantic model for patient data with validation and update function.

--- pydantic.py
+++ pydantic.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel ,EmailStr, AnyUrl, Field
 from typing import List ,Dict, Optional, Annotated
 
-class patient(BaseModel):
+class Patient(BaseModel):
     name : Annotated[str, Field( max_length=50, tittle = 'name of the patient', description='give the name of the patient in less than 50 words ')]
     email : EmailStr
     linkedin_url : AnyUrl
@@ -10,3 +10,15 @@ class patient(BaseModel):
     married: Annotated[bool, Field(default=None, description='Is the patient married or not')]
     allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact_details: Dict[str, str]
+    
+def update_patient_data(patient:patient):
+      print(patient.name)
+      print(patient.age)  
+      print(patient.allergies)  
+      print(patient.married)
+      print('updated')
+      
+patient_info = {'name':'piyush ', 'email':'piyush@gmail.com', 'linkedin_url' :'http://linked.com/1322', 'age':'30', 'weight': 75.2,'contact_details':{'phone':'2353462'}} 
+
+Patient1 = Patient(**patient_info)
+update_patient_data(Patient1)
