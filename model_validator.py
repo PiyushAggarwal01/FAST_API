01ab5fe960bd2d0642393a794d60d50085c5a1a4
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-09 13:31:46 +0530

    adfw

--- model_validator.py
+++ model_validator.py
@@ -29,6 +29,6 @@ def update_patient_data(patient: Patient):
 
 patient_info = {'name':'nitish', 'email':'abc@icici.com', 'age': '65', 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details':{'phone':'2353462', 'emergency':'235236'}}
 
-patient3 = Patient(**patient_info) 
+patient1 = Patient(**patient_info) 
 
-update_patient_data(patient3)
+update_patient_data(patient1)
