a71df367c64060e9917ec97ff765e33024818bb3
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-09 10:40:11 +0530

    wd

--- pydantic.py
+++ pydantic.py
@@ -5,4 +5,5 @@ class patient(BaseModel):
     name : Annotated[str, Field( max_length=50, tittle = 'name of the patient', description='give the name of the patient in less than 50 words ')]
     email : EmailStr
     linkedin_url : AnyUrl
-    age: Annotated[int , Field(gt=0, lt=120, title='age of the patient ', )]
+    age: Annotated[int , Field(gt=0, lt=120,  )]
+    weight : Annotated[float, Field(gt=0,strict=True)]
