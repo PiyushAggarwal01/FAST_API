4ef1c119dabe5a3d764b59dc6ea7e9d8a1e153e5
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-12 22:47:09 +0530

    bfrhbr

--- model_validator.py
+++ model_validator.py
@@ -13,7 +13,7 @@ class Patient(BaseModel):
 
     @model_validator(mode='after')
     def validate_emergency_contact(cls, model):
-        if model.age > 60 and 'emergency' not in model.contact_details:
+        if model.age > 61 and 'emergency' not in model.contact_details:
             raise ValueError('Patients older than 60 must have an emergency contact')
         return model
 
