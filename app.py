16772015059d726136ec2a35ab30b4f379e024a7
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-17 23:18:41 +0530

    xb

--- app.py
+++ app.py
@@ -32,3 +32,50 @@ class UserInput(Basemodel):
     occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
        'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
     
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight/(self.height**2)
+    
+    
+    @computed_field
+    @property
+    def lifestyle_risk(self)-> str:
+        if self.smoker and self.bmi > 30:
+            return"high"
+        elif self.smoker or self.bmi >27:
+            return "medium"
+        else :
+            return "low"
+        
+        
+    @computed_field
+    @property
+    def age_group(self)->str:
+        if self.age <25:
+            return "young"
+        elif self.age<45:
+            return "adult"
+        else :
+            return "senior"
+        
+    @computed_field
+    @property
+    def city_tier(self) ->str:
+        if self.cities in tier_1_cities:
+            return 1
+        if self.cities in tier_2_cities:
+            return 2 
+        else :
+            return 3
+        
+    @app.post('/predict')
+    def predict_premium(data: UserInput):
+        input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
