5f9abb0a23d6ccccf7017c53e66dae717357dd2e
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-17 22:43:37 +0530

    wfwf

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from fastapi import Fastapi
 from fastapi.responses import Jsonresponse
 from pydantic import Basemodel , Field , computed_field
 from typing import Literal , Annotated
-import Pickle 
+import pickle 
 import pandas as pd 
 
 # now importing tye model 
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = Fastapi()
+
