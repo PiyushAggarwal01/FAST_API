da1674a7e96f82800bd486dbdcb0fd4b67a9432b
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-17 22:48:00 +0530

    casdc

--- app.py
+++ app.py
@@ -23,5 +23,12 @@ tier_2_cities = [
 # pydantic model to validate incoming data 
 
 class UserInput(Basemodel):
-    age : Annotated[int , field(...,gt=0, lt =120, description='age of the user')]
+    age : Annotated[int , Field(...,gt=0, lt =120, description='age of the user')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the user')]
+    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Height of the user')]
+    income_lpa: Annotated[float, Field(..., gt=0, description='Annual salary of the user in lpa')]
+    smoker: Annotated[bool, Field(..., description='Is user a smoker')]
+    city: Annotated[str, Field(..., description='The city that the user belongs to')]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
     
