1376740bfe44f42835e4920144325f936a931747
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-17 22:39:18 +0530

    fefrg

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from fastapi import Fastapi
 from fastapi.responses import Jsonresponse
 from pydantic import Basemodel , Field , computed_field
+from typing import Literal , Annotated
+import Pickle 
+import pandas as pd 
+
+# now importing tye model 
