b97c9848593f5e67d7cce421c97554306278efc7
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-04-17 22:46:58 +0530

    wfw

--- app.py
+++ app.py
@@ -10,4 +10,18 @@ with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = Fastapi()
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
 
+# pydantic model to validate incoming data 
+
+class UserInput(Basemodel):
+    age : Annotated[int , field(...,gt=0, lt =120, description='age of the user')]
+    
